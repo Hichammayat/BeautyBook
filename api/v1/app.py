4f259e14f68732bbf7760913bb75d9db8f7eedeb
hichammayat <hichammayat07@gmail.com>
2024-03-01 18:31:06 +0100

    commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,41 @@
-from flask import Flask
-from api.v1.views import create_v1_blueprints
+#!/usr/bin/python3
+""" Flask Application """
+from models import storage
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
+# cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Configurations supplémentaires si nécessaire
-app.config['...'] = '...'
 
-# Initialisation et enregistrement des bleus de l'API v1
-create_v1_blueprints(app)
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'Beautybook Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
